16622395479242495ea682b73aff1cdbb82c1f1b
Sean Kelly <shonkelli@gmail.com>
2019-02-05 16:45:47 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,13 +56,14 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+ programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
+#     :languages => ["LISP", "C"]
 # }
 
 	programmer_hash = 
@@ -79,8 +81,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  
+  programmer_hash  
 end
 
 def changing_alan
@@ -101,8 +104,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
      
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +126,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     
+   programmer_hash[:dennis_ritchie][:languages].push("Assembly")  
+   
+  programmer_hash
 end
